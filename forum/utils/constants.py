34d86d255dca7d5e4e82ffc8781a9e3cc8478edb
Ciano Saraiva <saraiva.ufc@gmail.com>
2017-02-17 21:00:37 -0300

    Melhorias no latex

--- forum/utils/constants.py
+++ forum/utils/constants.py
@@ -5,5 +5,5 @@ from django.utils.translation import ugettext_lazy as _
 
 class Constants():
 	#Latex
-	HELP_TEXT_LATEX = _("Use <$ and $> to insert latex in text.\
-					Example: <$ ax^2 + bx + c = 0 $>")
+	HELP_TEXT_LATEX = _("Use $ and $ to insert latex in text.\
+					Example: $ ax^2 + bx + c = 0 $")
